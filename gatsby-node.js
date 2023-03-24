c41f45c9495928174f619292e9dec2f7718c29a0
AKM Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2023-03-24 06:10:56 +0600

    Change the js to tsx

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 // Define the template for blog post
-const blogPost = path.resolve(`./src/templates/blog-post.js`)
+const blogPost = path.resolve(`./src/templates/blog-post.tsx`)
 
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}

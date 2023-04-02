4d723864352900a39951ebae2fe062e5038503f4
AKM Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2023-04-02 22:49:13 +0600

    WIP

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -2,32 +2,15 @@ import * as React from "react";
 import { Link } from "gatsby";
 import Box from '@mui/material/Box';
 import { ThemeWrapper } from '../theme';
+import '../default-layout.css';
 
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
 
   return (
     <ThemeWrapper>
-      <div className="global-wrapper" data-is-root-path={isRootPath}>
-        <header className="global-header">{header}</header>
-        <main>
+      <div className="box">
+        <div className="element">
           <Box sx={{ justifyContent: 'center' }}>
             <Link to="/resume">Resume</Link>
             <Link to="/blog">Blog</Link>
@@ -35,9 +18,9 @@ const Layout = ({ location, title, children }) => {
 
             {children}
           </Box>
-        </main>
+        </div>
       </div>
-    </ThemeWrapper>
+    </ThemeWrapper >
   )
 }
 
